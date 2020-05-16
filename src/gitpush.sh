d3426f16f53d89cd19a01a942a08ff2be4fe838f
Jonathan Mo <jm9hx@virginia.edu>
2020-05-15 21:49:45 -0400

    Updated htmls and graphs as of 2020-05-15

--- src/gitpush.sh
+++ src/gitpush.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python3 /src/graphScript.py 
-python3 /src/write_to_html.py  
+./graphScript.py 
+./write_to_html.py  
 
 date=$(date +%F)
-cd ..
+
 git add -A
 
 git commit -m "Updated htmls and graphs as of $date"
