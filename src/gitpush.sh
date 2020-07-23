ed382199908b2125639d0347e2896edaaf1236e3
Jonathan Mo <jm9hx@virginia.edu>
2020-07-23 11:20:06 -0400

    updated README.md and added requirements.txt

--- src/gitpush.sh
+++ src/gitpush.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-./test.py 
-./write_to_html.py  
+START_TIME=$(date +%s)
+
+./graphScript.py
+EXIT_CODE=$?
+while [ $EXIT_CODE != 0 ]
+do
+    ./graphScript.py
+    EXIT_CODE=$?
+done
+
+./write_to_html.py
+EXIT_CODE=$?
+while [ $EXIT_CODE != 0 ]
+do
+    ./write_to_html.py
+    EXIT_CODE=$?
+done
 
 date=$(date +%F)
 time=$(date +"%T")
@@ -11,3 +26,7 @@ git add -A
 git commit -m "Updated htmls and graphs as of $date at $time"
 
 git push
+
+END_TIME=$(date +%s)
+RUNTIME=$((END_TIME-START_TIME))
+echo "Ran graphScript.py, write_to_html.py and pushed to respository in $RUNTIME seconds."
